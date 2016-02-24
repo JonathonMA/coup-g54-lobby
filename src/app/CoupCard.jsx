9726850886fdec2742e338a5c593e29fa806a417
Jonathon M. Abbott <jma@dandaraga.net>
2016-02-24 23:15:16 +1000

    Add category colors

--- src/app/CoupCard.jsx
+++ src/app/CoupCard.jsx
@@ -4,13 +4,27 @@ import Card from 'material-ui/lib/card/card'
 import CardHeader from 'material-ui/lib/card/card-header'
 import CardText from 'material-ui/lib/card/card-text'
 
+import colors from "material-ui/lib/styles/colors"
+
+let categoryColors = {
+  Communications: colors.blue500,
+  Finance: colors.green500,
+  Force: colors.purple500,
+  "Special Interests": colors.pink500
+}
+function colorOf (role) {
+  return categoryColors[role.category]
+}
+
 function CoupCard (props) {
-  let role = getRole(props.role)
+  let role = getRole(props.role),
+      cardColor = colorOf(role)
 
   return <Card>
     <CardHeader
       title={role.name}
       subtitle={role.category}
+      subtitleColor={cardColor}
     />
     <CardText>
       {role.text}
