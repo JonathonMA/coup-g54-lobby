4d6fc9ad682905157abac0f960b2ca224cb46cd5
Jonathon M. Abbott <jma@dandaraga.net>
2016-02-24 22:04:53 +1000

    Implement correct pregame rule

--- src/app/generate-coup-roles.js
+++ src/app/generate-coup-roles.js
@@ -1,8 +1,20 @@
+import entries from 'object.entries' // fuck off javascript you useless piece of shit
+import rolesInCategory from './roles-in-category'
+import shuffleArray from 'shuffle-array'
+
+let regularGame = {
+  Communications: 1,
+  Finance: 1,
+  Force: 1,
+  "Special Interests": 2
+}
+
+function pickCards(arr, [category, count]) {
+  return arr.concat(shuffleArray.pick(rolesInCategory(category), {picks: count}))
+}
+
 function generateCoupRoles () {
-  return [
-    "Farmer",
-    "Crime Boss"
-  ]
+  return entries(regularGame).reduce(pickCards, [])
 }
 
 export default generateCoupRoles
