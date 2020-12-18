32269832a3e495d80271806fb511ec7b7553e74c
Jonathon M. Abbott <jma@dandaraga.net>
2020-12-18 15:32:54 +1000

    Hookup App to Game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import CoupG4Game from './app/CoupG54Game'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <CssBaseline />
+      <CoupG4Game />
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
