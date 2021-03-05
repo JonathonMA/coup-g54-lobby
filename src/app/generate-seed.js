8d835949ca271ad248864957df70b21f25111c9d
Jonathon M. Abbott <jma@dandaraga.net>
2021-03-05 16:10:28 +1000

    Refactor generateSeed to use Chance

--- src/app/generate-seed.js
+++ src/app/generate-seed.js
@@ -1,7 +1,10 @@
-import sha1 from 'sha1'
+import Chance from 'chance'
 
 function generateSeed() {
-  return sha1(Math.random()).slice(0, 8)
+  // by default G54 has 5400 possible role configurations, so we should pick a hash size that could reasonable contain all these states
+  // (5C1 communications * 5C1 finance * 6C1 force * 9C2 special interests)
+  // if we wanted to be real clever we could calculate an appropriate hash size on the fly based on the roles database
+  return new Chance().hash({length: 4})
 }
 
 export default generateSeed
