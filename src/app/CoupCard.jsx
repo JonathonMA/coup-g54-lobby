a374c14672d8eef09fbc338b81797c8a4cda5606
Jonathon M. Abbott <jma@dandaraga.net>
2023-06-25 18:29:48 +1000

    mui: Switch to sx for theming

--- src/app/CoupCard.jsx
+++ src/app/CoupCard.jsx
@@ -1,17 +1,6 @@
 import React from "react"
 import { getRole } from "./coup"
-import { makeStyles } from "@mui/styles"
-import {
-  Avatar,
-  Card,
-  CardHeader,
-  CardContent,
-  StyledEngineProvider,
-  ThemeProvider,
-  createTheme,
-  colors,
-} from "@mui/material"
-import mapObject from "underscore/modules/mapObject"
+import { Avatar, Card, CardHeader, CardContent, colors } from "@mui/material"
 
 const roleColors = {
   communications: colors.blue[500],
@@ -20,48 +9,34 @@ const roleColors = {
   "special-interests": colors.pink[500],
 }
 
-const allRoles = (func) => mapObject(roleColors, func)
-
-const theme = createTheme()
-
-const useStyles = makeStyles((theme) =>
-  allRoles((color) => ({
-    "margin-top": "5px",
-    "& div.MuiAvatar-colorDefault": {
-      backgroundColor: color,
-    },
-    "& .roleFg": {
-      color: color,
-    },
-  }))
-)
 function CoupCard({ roleName }) {
   const role = getRole(roleName)
-  const classes = useStyles()
+  const roleColor = roleColors[role.category.id]
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <Card className={classes[role.category.id]}>
-          <CardHeader
-            avatar={<Avatar>{role.category.abbreviation}</Avatar>}
-            title={<div className="roleFg">{role.name}</div>}
-            subheader={role.category.name}
-          />
-          <CardContent>
-            <p>
-              <b>{role.action}</b>: {role.text}
-            </p>
-            {role.block && (
-              <p className="roleFg">
-                Blocks {role.action}
-                {role.special && <> ({role.special})</>}
-              </p>
-            )}
-          </CardContent>
-        </Card>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <Card sx={{ mt: "5px" }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: roleColor }}>
+            {role.category.abbreviation}
+          </Avatar>
+        }
+        sx={{ color: roleColor }}
+        title={<div className="roleFg">{role.name}</div>}
+        subheader={role.category.name}
+      />
+      <CardContent>
+        <p>
+          <b>{role.action}</b>: {role.text}
+        </p>
+        {role.block && (
+          <p className="roleFg">
+            Blocks {role.action}
+            {role.special && <> ({role.special})</>}
+          </p>
+        )}
+      </CardContent>
+    </Card>
   )
 }
 
